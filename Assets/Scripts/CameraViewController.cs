ae87e80b8893a68cf7610eec5a8696c8b54c7ae8
Victor Doktare <victor@doktare.com>
2023-03-06 15:53:37 +0100

    Project Setup - Unity 2021.3.20f1
    - Shader created for ground texture (Textures borrowed from VFX Apprentice)
    - Scene setup with basic postprocess and lightning

---
+++ Assets/Scripts/CameraViewController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraViewController : MonoBehaviour
+{
+    [SerializeField] private CameraAngle CameraType;
+    private enum CameraAngle {TopDown, Sideview}
+
+    void Update()
+    {
+        switch (CameraType)
+        {
+            case CameraAngle.TopDown:
+                transform.position = new Vector3(-8, 20, -8);
+                transform.rotation = Quaternion.Euler(60, 45, 0);
+                break;
+            case CameraAngle.Sideview:
+                transform.position = new Vector3(0, 6, -20);
+                transform.rotation = Quaternion.Euler(10, 0, 0);
+                break;
+            default:
+                break;
+        }
+    }
+}
